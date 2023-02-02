3008fa9405b8bb3af993c2c50a837c312d30791a
Yaniru <2223009802@qq.com>
2023-02-02 10:45:16 +0800

    initial git

---
+++ .config/lvim/config.lua
@@ -0,0 +1,218 @@
+--[[
+ `lvim` is the global options object
+]]
+
+-- vim options
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 1
+vim.opt.relativenumber = true
+vim.opt.showcmd = true
+
+-- general
+lvim.log.level = "info"
+lvim.format_on_save = {
+  enabled = false,
+  pattern = "*.lua",
+  timeout = 1000,
+}
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+
+-- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
+lvim.leader = "space"
+-- -- add your own keymapping
+-- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+-- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+-- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
+-- -- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["t"] = {
+  name = "Trouble",
+  t = { "<cmd>TroubleToggle<cr>", "trouble" },
+  w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
+  d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+  q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+  l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+  r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+}
+
+lvim.builtin.which_key.mappings["sp"] = { "<cmd>Telescope projects<cr>", "projects" }
+
+local wk = require("which-key")
+wk.register({
+  ["g"] = {
+    p = {
+      name = "preview",
+      d = { "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", "definition" },
+      t = { "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>", "type definition" },
+      i = { "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>", "implementation" },
+      r = { "<cmd>lua require('goto-preview').goto_preview_references()<CR>", "references" },
+    },
+    P = { name = "close preview window", },
+  },
+})
+
+-- <cmd>lua require('goto-preview').close_all_win()<CR>
+
+-- Change theme settings
+lvim.colorscheme = "tokyonight-day"
+
+-- After changing plugin config exit and reopen LunarVim, Run :PackerSync
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+
+-- Automatically install missing parsers when entering buffer
+lvim.builtin.treesitter.auto_install = true
+
+-- lvim.builtin.treesitter.ignore_install = { "haskell" }
+
+-- -- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
+
+-- --- disable automatic installation of servers
+-- lvim.lsp.installer.setup.automatic_installation = false
+
+-- ---configure a server manually. IMPORTANT: Requires `:LvimCacheReset` to take effect
+-- ---see the full default list `:lua =lvim.lsp.automatic_configuration.skipped_servers`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
+-- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { command = "stylua" },
+--   {
+--     command = "prettier",
+--     extra_args = { "--print-with", "100" },
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
+-- }
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "flake8", filetypes = { "python" } },
+--   {
+--     command = "shellcheck",
+--     args = { "--severity", "warning" },
+--   },
+-- }
+--
+-- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
+lvim.plugins = {
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  {
+    "ggandor/lightspeed.nvim",
+    event = "BufRead",
+  },
+  {
+    "tzachar/cmp-tabnine",
+    build = "./install.sh",
+    dependencies = "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    sort = true,
+    run_on_every_keystroke = true,
+  },
+  {
+    "rmagatti/goto-preview",
+    config = function()
+      require('goto-preview').setup {
+        width = 120; -- Width of the floating window
+        height = 25; -- Height of the floating window
+        default_mappings = true; -- Bind default mappings
+        debug = false; -- Print debug information
+        opacity = 10; -- 0-100 opacity level of the floating window where 100 is fully transparent.
+        post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
+      }
+    end
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require "lsp_signature".on_attach({
+        handler_opts = {
+          border = "rounded" -- double, rounded, single, shadow, none, or a table of borders
+        },
+        transparency = 20, -- allow floating win transparent value 1~100
+        floating_window_above_cur_line = true,
+      })
+    end,
+  },
+  {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+      require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
+          '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+        hide_cursor = true, -- Hide cursor while scrolling
+        stop_eof = true, -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+        respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil, -- Default easing function
+        pre_hook = nil, -- Function to run before the scrolling animation starts
+        post_hook = nil, -- Function to run after the scrolling animation ends
+      })
+    end
+  },
+  { "tpope/vim-surround", },
+  { "tpope/vim-repeat" },
+  { "mg979/vim-visual-multi" },
+  {
+    "utilyre/barbecue.nvim",
+    version = "*",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    config = function()
+      require("barbecue").setup({
+        show_modified = true,
+        symbols = {
+          ---modification indicator
+          ---@type string
+          modified = "Modified",
+        },
+        custom_section = function() return "Be your own hero." end,
+      })
+    end,
+  },
+}
+
+vim.api.nvim_command("let g:VM_theme = 'purplegray'")
+
+-- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "zsh",
+--   callback = function()
+--     -- let treesitter use bash highlight for zsh files as well
+--     require("nvim-treesitter.highlight").attach(0, "bash")
+--   end,
+-- })
---
+++ .zshrc
@@ -0,0 +1,72 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/yaniru/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+
+# init ohmyzsh and theme
+source ~/.local/share/ohmyzsh-master/oh-my-zsh.sh
+# source ~/.local/share/ohmyzsh-master/themes/amuse.zsh-theme
+# source ~/.local/share/ohmyzsh-master/themes/afowler.zsh-theme
+source ~/.local/share/ohmyzsh-master/custom/themes/powerlevel10k/powerlevel10k.zsh-theme
+# plugins
+source ~/.local/share/ohmyzsh-master/plugins/gitignore/gitignore.plugin.zsh
+source ~/.local/share/ohmyzsh-master/plugins/z/z.plugin.zsh
+source ~/.local/share/ohmyzsh-master/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.local/share/ohmyzsh-master/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+eval $(thefuck --alias)
+
+alias proch="proxychains4"
+alias l="exa -al --icons"
+alias ls="exa -a --icons"
+alias trans="trans"
+alias transb="trans --brief"
+alias tranz="trans :zh-CN"
+alias tranzb="trans :zh-CN --brief"
+alias rm='echo "rm: This is not the command you are looking for."; false'
+alias trp="trash-put"
+alias trl="trash-list"
+alias trr="trash-restore"
+alias tre="trash-empty --trash-dir=~/.local/share/Trash"
+alias trm="trash-rm"
+alias s="screenfetch"
+alias fzfb="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+alias ff="fuck"
+alias le="leetcode"
+alias e="exit"
+alias q="exit"
+alias ra="ranger"
+alias conf='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+export PATH=/opt/others/bin:/home/yaniru/.local/bin/myscripts:$PATH
+export NEMU_HOME=/home/yaniru/workshop/projects/ics2021/nemu
+export AM_HOME=/home/yaniru/workshop/projects/ics2021/abstract-machine
+export REPO_DIR=/home/yaniru/workshop/projects/CS61B-sp21
+
+# config proxy
+# export http_proxy='127.0.0.1:9981'
+# export https_proxy='http://127.0.0.1:9981'
+
+# config lvim path
+export LUNARVIM_RUNTIME_DIR="${LUNARVIM_RUNTIME_DIR:-"/home/yaniru/.local/share/lunarvim"}"
+export LUNARVIM_CONFIG_DIR="${LUNARVIM_CONFIG_DIR:-"/home/yaniru/.config/lvim"}"
+export LUNARVIM_CACHE_DIR="${LUNARVIM_CACHE_DIR:-"/home/yaniru/.cache/lvim"}"
+export LUNARVIM_BASE_DIR="${LUNARVIM_BASE_DIR:-"/home/yaniru/.local/share/lunarvim/lvim"}"
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
